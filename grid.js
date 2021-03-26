5450b84444e6a5c70d576df3f21803b9e40353bd
chrisboccia <80723413+chrisboccia@users.noreply.github.com>
2021-03-26 03:44:54 -0400

    roads added
    added some on-click instructions for roads

--- grid.js
+++ grid.js
@@ -48,17 +48,49 @@ var row = grid.selectAll(".row")
 var column = row.selectAll(".square")
 	.data(function(d) { return d; })
 	.enter().append("rect")
-	.attr("class","square")
+	.attr("general","square")
 	.attr("x", function(d) { return d.x; })
 	.attr("y", function(d) { return d.y; })
 	.attr("width", function(d) { return d.width; })
 	.attr("height", function(d) { return d.height; })
 	.style("fill", "#fff")
-	.style("stroke", "#F56C4E")
-	.on('click', function(d) {
-       d.click ++;
-       if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); d.treatment=0; }
-	   if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); d.treatment=1; }
-	   if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); d.treatment=2;}
-	   if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); d.treatment=3;}
-    });
+	.style("stroke", "#EB984E")
+	.style("stroke-width", 4)
+	.on('click', function(d2) {
+       d2.click ++;
+       if ((d2.click)%4 == 0 ) { d3.select(this).style("fill","#66ccff"); d2.treatment=0; }
+	   if ((d2.click)%4 == 1 ) { d3.select(this).style("fill","#2ECC71"); d2.treatment=1; }
+	   if ((d2.click)%4 == 2 ) { d3.select(this).style("fill","#D5F5E3"); d2.treatment=2;}
+	   if ((d2.click)%4 == 3 ) { d3.select(this).style("fill","#F7DC6F"); d2.treatment=3;}
+    })
+	.on("contextmenu", function (d, i) {
+        d3.event.preventDefault();
+		d.click ++;
+        if ((d.click)%11 == 0 ) {d3.select(this).style("stroke-dasharray","0").style("stroke","#EB984E"); d.treatment=0; }
+		if ((d.click)%11 == 1 ) {d3.select(this).style("stroke-dasharray", "0,150,50").style("stroke","black").style("stroke-width", 6); d.treatment=0; }
+		if ((d.click)%11 == 2 ) {d3.select(this).style("stroke-dasharray", "50,150").style("stroke","black").style("stroke-width", 6); d.treatment=1; }
+		if ((d.click)%11 == 3 ) {d3.select(this).style("stroke-dasharray", "0,100,50,100").style("stroke","black").style("stroke-width", 6); d.treatment=0; }
+		if ((d.click)%11 == 4 ) {d3.select(this).style("stroke-dasharray", "0,50, 50,50").style("stroke","black").style("stroke-width", 6); d.treatment=1; }
+		if ((d.click)%11 == 5 ) {d3.select(this).style("stroke-dasharray","50").style("stroke","black").style("stroke-width", 6); d.treatment=0; }
+		if ((d.click)%11 == 6 ) {d3.select(this).style("stroke-dasharray", "0,50,50,0" ).style("stroke","black").style("stroke-width", 6); d.treatment=1; }
+		if ((d.click)%11 == 7 ) {d3.select(this).style("stroke-dasharray", "0,100,150" ).style("stroke","black").style("stroke-width", 6); d.treatment=0; }
+		if ((d.click)%11 == 8 ) {d3.select(this).style("stroke-dasharray", "0,100,150" ).style("stroke","black").style("stroke-width", 6); d.treatment=1; }
+		if ((d.click)%11 == 9 ) {d3.select(this).style("stroke-dasharray", "0,50,50" ).style("stroke","black").style("stroke-width", 6); d.treatment=0; }
+		if ((d.click)%11 == 10 ) {d3.select(this).style("stroke-dasharray", "100").style("stroke","black").style("stroke-width", 6); d.treatment=1; }
+		if ((d.click)%11 == 11 ) {d3.select(this).style("stroke-dasharray", "50,100" ).style("stroke","black").style("stroke-width", 6); d.treatment=1; }
+        });
+
+//https://codepen.io/lazd/pen/WNweNwy?editors=1010
+//rect { fill: none; stroke: red; }
+//.topBottom { stroke-dasharray: 50 }
+//.leftRight { stroke-dasharray: 0,50,50,0 }
+
+//.bottomLeft { stroke-dasharray: 0,100,150 }
+//.bottomRight { stroke-dasharray: 0,50,50 }
+//.topRight { stroke-dasharray: 100 }
+//.topLeft { stroke-dasharray: 50,100 }
+
+//.left { stroke-dasharray: 0,150,50 }
+//.top { stroke-dasharray: 50,150 }
+//.bottom { stroke-dasharray: 0,100,50,100 }
+//.right { stroke-dasharray: 0,50, 50,50 }
